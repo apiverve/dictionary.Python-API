812192a80728dec0f7e2e26890f9f4d7d343373c
APIVerve Team <hello@apiverve.com>
2025-03-28 17:14:46 -0500

    Bug Fixes

--- src/apiverve_dictionary/apiClient.py
+++ src/apiverve_dictionary/apiClient.py
@@ -10,10 +10,10 @@ class DictionaryAPIClient:
         
         self.api_key = api_key
         self.secure = secure
-        self.base_url = 'https://api.apiverve.com/v1.0.9/dictionary'
+        self.base_url = 'https://api.apiverve.com/v1/dictionary'
         self.headers = {
             'x-api-key': self.api_key,
-            'auth-mode': 'pypi'
+            'auth-mode': 'pypi-package'
         }
 
     def execute(self, params=None):
